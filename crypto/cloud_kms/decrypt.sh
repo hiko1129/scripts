b15acf50704677c64f2aeaf54172e002fcabe244
Masaki Hikosaka <masaki_hikosaka@outlook.com>
2020-08-27 15:04:29 +0900

    updated

--- crypto/cloud_kms/decrypt.sh
+++ crypto/cloud_kms/decrypt.sh
@@ -4,4 +4,5 @@ keyring=$3
 file_paths=$4
 
 curl https://raw.githubusercontent.com/hiko1129/scripts/master/crypto/cloud_kms/base.sh > i.sh && \
-     sh i.sh decrypt $project_id $location $keyring $file_paths
+     sh i.sh decrypt $project_id $location $keyring $file_paths && \
+     rm i.sh
